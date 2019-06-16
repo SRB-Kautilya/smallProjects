696dfe1c5c1b9f04fb7d99a9ea3b2853a8b3dd5e
shashi <shashir867@gmail.com>
2019-06-16 16:42:31 -0400

    message code small changes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+//Assigning const values
 const sendBtn = document.querySelector('#sendBtn');
 const messageIn = document.querySelector('#messageIn')
 const messageOut = document.querySelector('#messageOut')
 
+
+// listenting to click event
 sendBtn.addEventListener('click',sendMsg);
 
+// when you click without text then it shows alert message or else it show the message you send
 function sendMsg () {
    var textin  = messageIn.value
    console.log('textin',textin)
